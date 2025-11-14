3f3d59499cf0da9caaec57825b11e3c1d8c8948f
Patrick Golden <ptgolden@email.unc.edu>
2025-11-14 14:43:08 -0500

    Highlight matching text in search results

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,28 +1,35 @@
 import { useState, useEffect } from "react";
 import { useSearchEngine, SearchResultWithNode } from "../search";
 import { GraphNode } from "../graph";
+import Highlighter from "react-highlight-words";
 
 type TermSearchProps<T extends GraphNode = GraphNode> = {
   nodes: Array<T>;
   onSelectNode?: (node: T) => void;
-}
+};
 
 export default function TermSearch<T extends GraphNode = GraphNode>(
   props: TermSearchProps<T>,
-){
-  const [ results, setResults ] = useState<Array<SearchResultWithNode<T>> | null>(null)
-  const [ searchText, setSearchText ] = useState("");
+) {
+  const [results, setResults] = useState<Array<SearchResultWithNode<T>> | null>(
+    null,
+  );
+  const [searchText, setSearchText] = useState("");
   const { engine } = useSearchEngine(props.nodes);
 
   useEffect(() => {
     const results = engine.search(searchText, {
       prefix: true,
-      boost: { "label": 2 },
+      boost: { label: 2 },
       combineWith: "and",
-    })
+    });
 
-    setResults(results.slice(0, 50))
-  }, [ searchText ])
+    setResults(results.slice(0, 50));
+  }, [searchText]);
+
+  const highlightStrings = searchText
+    .split(" ")
+    .map(word => new RegExp("\\b" + word));
 
   return (
     <div>
@@ -38,18 +45,42 @@ export default function TermSearch<T extends GraphNode = GraphNode>(
 
       <div>
         {(results || []).map(result => (
-          <div key={result.id}>
-            { result.node.label } - { result.score }
+          <div
+            key={result.id}
+            style={{
+              cursor: "pointer",
+            }}
+            onClick={() => {
+              if (props.onSelectNode) {
+                props.onSelectNode(result.node);
+              }
+            }}
+          >
+            <Highlighter
+              textToHighlight={result.node.label || ""}
+              searchWords={highlightStrings}
+            />{" "}
+            - {result.score}
             {result.node.synonyms.map(syn => (
-              <div>{ syn.value }</div>
+              <div>
+                <Highlighter
+                  textToHighlight={syn.value}
+                  searchWords={highlightStrings}
+                />
+              </div>
             ))}
             {result.node.definitions.map(def => (
-              <div>{ def.value }</div>
+              <div>
+                <Highlighter
+                  textToHighlight={def.value}
+                  searchWords={highlightStrings}
+                />
+              </div>
             ))}
             <hr />
           </div>
         ))}
       </div>
     </div>
-  )
+  );
 }
