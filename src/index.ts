b85ed245a51ba8affcc83a4d9b8b7ac7b60d9c3a
Patrick Golden <ptgolden@email.unc.edu>
2025-12-03 11:20:23 -0500

    Clean up public exports from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
-export { default as Graph, GraphNode, Hierarchy } from "./graph";
+// React
 export { default as HierarchyTree } from "./components/Hierarchy";
+export { useNodeSearch } from "./search";
+
+// Data model
+export { default as Graph } from "./graph";
+export { default as Hierarchy } from "./hierarchy";
+export { default as Path } from "./path";
+
+// Loaders
 export { default as OBOGraphLoader } from "./loaders/obograph";
+
+// Types
+export type { GraphNode } from "./types";
 export type { OBOGraphNode } from "./loaders/obograph";
